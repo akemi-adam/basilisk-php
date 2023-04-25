f887c787a9869fca1ee4014a2074de4aa015d824
Akemi Adam <90357785+akemi-adam@users.noreply.github.com>
2023-04-25 20:45:50 -0300

    #[0.0.4] Add documents in web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,21 @@ use AkemiAdam\Basilisk\App\Kernel\Route;
 
 $route = new Route;
 
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| This is where you can define the routes of your application.
+|
+| To do this, simply call one of the HTTP verbs (get and post) from the $router object and
+| pass the path and callback as arguments.
+|
+| Example:
+|
+| $router->get('/', function () => { return view('dashboard'); });
+|
+*/
 
 $route->get('/', function ()
 {
