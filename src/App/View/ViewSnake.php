c3bf30b52a8e709f187bdb2733ad3c3329ceb9da
akemi-adam <mosiah.adam@escolar.ifrn.edu.br>
2023-08-03 14:31:13 -0300

    #[0.1] New feature: my own template engine called Snake

--- src/App/View/ViewSnake.php
+++ src/App/View/ViewSnake.php
@@ -2,18 +2,16 @@
 
 namespace AkemiAdam\Basilisk\App\View;
 
-use Twig\Environment;
-use Twig\Loader\FilesystemLoader;
+use AkemiAdam\Basilisk\App\Kernel\Config;
+
 
 class ViewSnake
 {
-    private Environment $twig;
+    private string $path;
 
     public function __construct(string $path = '')
     {
-        $path = $path ?: views_path();
-
-        $this->twig = new Environment(new FilesystemLoader($path));
+        $this->path = $path ?: views_path();
     }
 
     /**
@@ -24,8 +22,126 @@ class ViewSnake
      * 
      * @return void
      */
-    public function render(string $filename, array $data = []) : void
+    public function render(string $filename, array $data = [])
     {
-        echo $this->twig->render($filename, $data);
+        foreach ($data as $key => $value) ${$key} = $value;
+
+        $viewPath = \storage_path() . '/views';
+
+        $originalFile = "{$this->path}/$filename";
+
+        $configPath = "$viewPath/views.json";
+        
+        // Checks if the template exists and if it has been modified
+        if ($view = Config::getSetting($configPath, $filename))
+        {
+            if (!$this->fileWasModified($originalFile, "$viewPath/$view.php"))
+            {
+                include "$viewPath/$view.php";
+
+                exit;
+            }
+
+            unlink("$viewPath/$view.php");
+        }
+
+        $newFilename = md5($originalFile);
+
+        $file = fopen("$viewPath/$newFilename.php", 'w');
+
+        Config::addSetting($configPath, $filename, $newFilename);
+
+        $this->make($originalFile, $file);
+
+        fclose($file);
+
+        include "$viewPath/$newFilename.php";
+    }
+
+    /**
+     * Checks if a file been was modified
+     * 
+     * @param string $originalFile
+     * @param string $newFile
+     * 
+     * @return bool
+     */
+    private function fileWasModified(string $originalFile, string $newFile) : bool
+    {
+        $originalContents = file_get_contents($originalFile);
+        
+        $newContents = str_replace(['<?=', '?>'], ['{{', '}}'], file_get_contents($newFile));
+
+        return $originalContents !== $newContents;
+    }
+
+    /**
+     * Reads the template file line by line and defines the data and the logic
+     * 
+     * @param $originalFile
+     * @param $file
+     * 
+     * @return void
+     */
+    private function make(string $originalFile, $file) : void
+    {
+        foreach (\read_file($originalFile) as $line)
+        {
+            if (preg_match('/{{|}}/', $line))
+                $this->setData($line);
+
+            if (preg_match('/@/', $line))
+            {
+                $this->setBlock('if', $line, function (string &$line) {
+                    if (preg_match('/@elseif\(.*\)/', $line)) {
+                        $line = preg_replace('/@/', '<?php ', $line);
+                        $line = preg_replace('/elseif\((.*?)\)/', 'elseif ($1): ?>', $line);
+                    }
+
+                    if (preg_match('/@else/', $line))
+                        $line = str_replace('@else', '<?php else: ?>', $line);
+                });
+
+                $this->setBlock('foreach', $line);
+                
+                $this->setBlock('for', $line);
+            }
+
+            fputs($file, $line);
+        }
+    }
+
+    /**
+     * Defines a data in the template to be shown
+     * 
+     * @param string &$line
+     * 
+     * @return void
+     */
+    private function setData(string &$line) : void
+    {
+        $line = str_replace([ '{{', '}}' ], [ '<?=', '?>' ], $line);
+    }
+
+    /**
+     * Defines logic struct in the template
+     * 
+     * @param string &$line
+     * 
+     * @return void
+     */
+    private function setBlock(string $struct, string &$line, ?\Closure $othersChecks = null)
+    {
+        if (preg_match("/@$struct\(.*\)/", $line))
+        {
+            $line = preg_replace("/@/", "<?php ", $line);
+
+            $line = preg_replace("/$struct\((.*?)\)/", "$struct ($1): ?>", $line);
+        }
+
+        if ($othersChecks) $othersChecks($line);
+
+        if (preg_match("/@end$struct/", $line))
+            $line = str_replace("@end$struct", "<?php end$struct; ?>", $line);
     }
 }
